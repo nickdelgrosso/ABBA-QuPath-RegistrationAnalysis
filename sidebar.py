c524596ddeefe9849ea56448be0d8c7897920300
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-09-04 16:37:09 +0200

    multiple filenames

--- sidebar.py
+++ sidebar.py
@@ -21,14 +21,19 @@ class Sidebar(HasWidget):
         button.clicked.connect(self.load_data)
 
     def load_data(self):
-        filename, filetype = QFileDialog.getOpenFileName(
+        filenames, filetype = QFileDialog.getOpenFileNames(
             parent=self._widget,
             caption="Load Cell Points from File",
             # dir="D:/QuPath Projects/Project3/export2",
             filter="TSV Files (*.tsv)"
         )
-        if not filename:
+
+        if not filenames:
             return
+        filename, *other_filenames = filenames
+        if other_filenames:
+            raise NotImplementedError("Multiple Filenames not yet implemented")
+
         df = read_detection_file(filename=filename, atlas=self.model.atlas)
         self.model.cells = df
 
