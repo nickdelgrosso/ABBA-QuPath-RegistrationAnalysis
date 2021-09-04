e85e74288d548281c6bdb676cb806c8072896dc3
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-09-04 02:56:53 +0200

    moved model into PlotterWindow

--- app.py
+++ app.py
@@ -1,32 +1,13 @@
 import sys
 
-import pandas as pd
 from PyQt5.QtWidgets import QApplication
 
 from bg_atlasapi import BrainGlobeAtlas
 
-from model import model
+from model import model, read_detection_file
 from plot_3d import PlotterWindow
 from main_window import MainWindow
 from region_tree import BrainRegionTree
-
-
-def read_detection_file(filename: str, atlas: BrainGlobeAtlas) -> pd.DataFrame:
-    return (
-        pd.read_csv(filename, sep='\t')
-            .rename(columns={'Allen CCFv3 X mm': 'X', 'Allen CCFv3 Y mm': 'Y', 'Allen CCFv3 Z mm': 'Z'})
-            .astype({'X': float, 'Y': float, 'Z': float})
-            .pipe(lambda df: df.assign(
-            BGIdx=atlas.annotation[
-                (df.X / (atlas.resolution[0] / 1000)).values.astype(int),
-                (df.Y / (atlas.resolution[1] / 1000)).values.astype(int),
-                (df.Z / (atlas.resolution[2] / 1000)).values.astype(int),
-            ]
-        ))
-            .merge(atlas.lookup_df, right_on="id", left_on="BGIdx", how="left")
-            .astype({'acronym': 'category', 'name': 'category'})
-            .drop(columns=['id'])
-    ).sample(10000)
 
 
 if __name__ == '__main__':
@@ -34,13 +15,7 @@ if __name__ == '__main__':
     win = MainWindow(
         main_widgets=[
             BrainRegionTree(model=model),
-            PlotterWindow(
-                cells=read_detection_file(
-                    filename='D:/QuPath Projects/Project3/export2/PW166-A14_Scan1_[4314,45057]_component_data_merged_Region 2.ome.tif__detections2.tsv',
-                    atlas=(atlas := BrainGlobeAtlas("allen_mouse_25um")),
-                ),
-                atlas=atlas,
-            ),
+            PlotterWindow(model=model),
         ]
     )
     win.show()
--- model.py
+++ model.py
@@ -1,16 +1,40 @@
+import pandas as pd
 from bg_atlasapi import BrainGlobeAtlas
 from traitlets import HasTraits, Instance, observe, Tuple, Int
 
 
+def read_detection_file(filename: str, atlas: BrainGlobeAtlas) -> pd.DataFrame:
+    return (
+        pd.read_csv(filename, sep='\t')
+            .rename(columns={'Allen CCFv3 X mm': 'X', 'Allen CCFv3 Y mm': 'Y', 'Allen CCFv3 Z mm': 'Z'})
+            .astype({'X': float, 'Y': float, 'Z': float})
+            .pipe(lambda df: df.assign(
+            BGIdx=atlas.annotation[
+                (df.X / (atlas.resolution[0] / 1000)).values.astype(int),
+                (df.Y / (atlas.resolution[1] / 1000)).values.astype(int),
+                (df.Z / (atlas.resolution[2] / 1000)).values.astype(int),
+            ]
+        ))
+            .merge(atlas.lookup_df, right_on="id", left_on="BGIdx", how="left")
+            .astype({'acronym': 'category', 'name': 'category'})
+            .drop(columns=['id'])
+    ).sample(10000)
+
+
 class AppState(HasTraits):
     atlas = Instance(BrainGlobeAtlas)
-    selected_region_ids = Tuple(default_value=())  # should be tuple of ints
+    cells = Instance(klass=pd.DataFrame)
 
+    selected_region_ids = Tuple(default_value=())  # should be tuple of ints
     @observe('selected_region_ids')
     def _on_change_selected_region_ids(self, change):
         print(f"Selected: {change['new']}")
 
 
 model = AppState(
-    atlas=BrainGlobeAtlas("allen_mouse_25um")
+    atlas=BrainGlobeAtlas("allen_mouse_25um"),
+    cells=read_detection_file(
+        filename='D:/QuPath Projects/Project3/export2/PW166-A14_Scan1_[4314,45057]_component_data_merged_Region 2.ome.tif__detections2.tsv',
+        atlas=BrainGlobeAtlas("allen_mouse_25um"),
+    ),
 )
