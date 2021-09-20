836b638f1bb54a53bdf82f7d86f78b26827facc1
Estelle Nassar <estelle.nassar@crick.ac.uk>
2021-09-20 17:58:21 +0100

    We split the lambda function into a new function, "get_brain_region_label", refactoring the inputs, and used np.clip to make sure the indices are within the annotation atlas range

--- data/load_data.py
+++ data/load_data.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+import numpy as np
 import pandas as pd
 from bg_atlasapi import BrainGlobeAtlas
 
@@ -7,16 +10,20 @@ def read_detection_file(filename: str, atlas: BrainGlobeAtlas) -> pd.DataFrame:
     print(df.columns)
     return (
         df
-        .rename(columns={'Allen CCFv3 X mm': 'X', 'Allen CCFv3 Y mm': 'Y', 'Allen CCFv3 Z mm': 'Z'})
-        .astype({'X': float, 'Y': float, 'Z': float})
-        .pipe(lambda df: df.assign(
-            BGIdx=atlas.annotation[
-                (df.X / (atlas.resolution[0] / 1000)).values.astype(int),
-                (df.Y / (atlas.resolution[1] / 1000)).values.astype(int),
-                (df.Z / (atlas.resolution[2] / 1000)).values.astype(int),
-            ]
-        ))
-        .merge(atlas.lookup_df, right_on="id", left_on="BGIdx", how="left")
-        .astype({'acronym': 'category', 'name': 'category'})
-        .drop(columns=['id'])
-    )#.sample(10000)
+            .rename(columns={'Allen CCFv3 X mm': 'X', 'Allen CCFv3 Y mm': 'Y', 'Allen CCFv3 Z mm': 'Z'})
+            .astype({'X': float, 'Y': float, 'Z': float})
+            .pipe(lambda df: df.assign(BGIdx=get_brain_region_label(df.X.values, df.Y.values, df.Z.values, annotation=atlas.annotation, resolution=atlas.resolution)))
+            .merge(atlas.lookup_df, right_on="id", left_on="BGIdx", how="left")
+            .astype({'acronym': 'category', 'name': 'category'})
+            .drop(columns=['id'])
+    )  # .sample(10000)
+
+
+def get_brain_region_label(x, y, z, annotation: np.ndarray, resolution: Tuple[int, int, int]) -> np.ndarray:
+    sx, sy, sz = annotation.shape
+    rx, ry, rz = resolution
+    return annotation[
+        np.clip((x / (rx / 1000)).astype(int), 0, sx-1),
+        np.clip((y / (ry / 1000)).astype(int), 0, sy-1),
+        np.clip((z / (rz / 1000)).astype(int), 0, sz-1),
+    ]
