0091726a86e14dd6ba227835a6a72244093d5159
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-09-23 23:50:54 +0100

    added short readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# ABBA-QuPath Registration Exporter
+
+A Python app and Groovy script that helps export transformed cell detections
+from ABBA-registered data stored in QuPath projects.
+
+## Installation
--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.readlines()
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='ABBA-QuPath-RegistrationExporter',
     version='0.1',
     packages=find_packages(),
     url='',
     license='MIT',
-    author='nickdg',
-    author_email='Nicholas A. Del Grosso',
+    author='Nicholas A. Del Grosso',
+    author_email='delgrosso.nick@gmail.com',
     description='A QuPath-Abba Registration Exporter Application for Mouse Sections',
+    long_description=long_description,
     install_requires=requirements,
     entry_points={'console_scripts':[
         "regexport=regexport.app:main"
