20818f872acee1cca12bade58bab23d17cbaa109
Estelle Nassar <estelle.nassar@crick.ac.uk>
2021-09-23 15:03:48 +0100

    removed BGIdx

--- data/load_data.py
+++ data/load_data.py
@@ -15,8 +15,11 @@ def read_detection_file(filename: Path, atlas: BrainGlobeAtlas) -> pd.DataFrame:
                 'Allen CCFv3 X mm': float,
                 'Allen CCFv3 Y mm': float,
                 'Allen CCFv3 Z mm': float,
-            })
-            .pipe(lambda df: df.assign(BGIdx=get_brain_region_label(df['Allen CCFv3 X mm'].values, df['Allen CCFv3 Y mm'].values, df['Allen CCFv3 Z mm'].values, annotation=atlas.annotation, resolution=atlas.resolution)))
+        })
+            .pipe(lambda df: df.assign(
+                BGIdx=get_brain_region_label(df['Allen CCFv3 X mm'].values, df['Allen CCFv3 Y mm'].values,
+                                             df['Allen CCFv3 Z mm'].values, annotation=atlas.annotation,
+                                            resolution=atlas.resolution)))
             .merge(atlas.lookup_df, right_on="id", left_on="BGIdx", how="left")
             .rename(columns={
                 'Allen CCFv3 X mm': 'X',
@@ -24,8 +27,8 @@ def read_detection_file(filename: Path, atlas: BrainGlobeAtlas) -> pd.DataFrame:
                 'Allen CCFv3 Z mm': 'Z',
                 'name': 'BrainRegion',
                 'acronym': 'Acronym',
-            })
-            .drop(columns=['id'])
+        })
+            .drop(columns=['id', 'BGIdx'])
             .assign(Image=filename.name)
     )  # .sample(10000)
 
@@ -34,9 +37,9 @@ def get_brain_region_label(x, y, z, annotation: np.ndarray, resolution: Tuple[in
     sx, sy, sz = annotation.shape
     rx, ry, rz = resolution
     return annotation[
-        np.clip((x / (rx / 1000)).astype(int), 0, sx-1),
-        np.clip((y / (ry / 1000)).astype(int), 0, sy-1),
-        np.clip((z / (rz / 1000)).astype(int), 0, sz-1),
+        np.clip((x / (rx / 1000)).astype(int), 0, sx - 1),
+        np.clip((y / (ry / 1000)).astype(int), 0, sy - 1),
+        np.clip((z / (rz / 1000)).astype(int), 0, sz - 1),
     ]
 
 
