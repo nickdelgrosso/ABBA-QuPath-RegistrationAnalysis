bb4c344f91fe50f52efcb3f935d378f764b0288f
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-09-27 19:25:20 +0100

    created App class for simpler testing

---
+++ regexport/app.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass, field, fields, Field
+
+from PyQt5.QtWidgets import QMainWindow
+
+from regexport.actions.download_biop_extensions import SaveBiopExtensionsAction, SaveBiopExtensionsActionModel
+from regexport.actions.load_atlas import LoadAtlasActionModel, LoadAtlasAction
+from regexport.actions.load_cells import LoadCellsActionModel, LoadCellsAction
+from regexport.actions.save_cells import SaveCellsActionModel, SaveCellsAction
+from regexport.actions.save_script import SaveGroovyScriptActionModel, SaveGroovyScriptAction
+from regexport.model import AppState
+from regexport.views.histogram import HistogramModel, HistogramView
+from regexport.views.main_window import MainWindow
+from regexport.views.plot_3d import PlotterModel, PlotterView
+from regexport.views.region_tree import BrainRegionTreeViewModel, BrainRegionTree
+from regexport.views.sidebar import Sidebar
+from regexport.views.text_selector import TextSelectorModel, DropdownTextSelectorView
+
+
+class App:
+    def __init__(self):
+        self.model = AppState()
+
+        self.plotter_model = PlotterModel()
+        self.plotter_model.register(model=self.model)
+
+        self.brain_region_tree_model = BrainRegionTreeViewModel()
+        self.brain_region_tree_model.register(model=self.model)
+
+        self.load_atlas_action_model = LoadAtlasActionModel()
+        self.load_atlas_action_model.register(model=self.model)
+
+        self.load_cells_action_model = LoadCellsActionModel()
+        self.load_cells_action_model.register(model=self.model)
+
+        self.save_cells_action_model = SaveCellsActionModel()
+        self.save_cells_action_model.register(model=self.model)
+
+        self.colormap_selector_model = TextSelectorModel()
+        self.colormap_selector_model.register(model=self.model, options_attr='colormap_options',
+                                              selected_attr='selected_colormap')
+
+        self.colordata_selector_model = TextSelectorModel()
+        self.colordata_selector_model.register(model=self.model, options_attr='column_to_plot_options',
+                                               selected_attr='column_to_plot')
+
+        self.histogram_model = HistogramModel()
+        self.histogram_model.register(model=self.model)
+
+        self.save_biop_extensions_button = SaveBiopExtensionsActionModel()
+        self.save_groovy_script_button = SaveGroovyScriptActionModel()
+
+    def create_gui(self) -> QMainWindow:
+        return MainWindow(
+            main_widgets=[
+                BrainRegionTree(model=self.brain_region_tree_model),
+                PlotterView(model=self.plotter_model),
+                Sidebar(
+                    widgets=[
+                        DropdownTextSelectorView(model=self.colordata_selector_model),
+                        DropdownTextSelectorView(model=self.colormap_selector_model),
+                        HistogramView(model=self.histogram_model),
+                    ]
+                ),
+            ],
+            menu_actions=[
+                SaveBiopExtensionsAction(model=self.save_biop_extensions_button),
+                SaveGroovyScriptAction(model=self.save_groovy_script_button),
+                LoadAtlasAction(model=self.load_atlas_action_model),
+                LoadCellsAction(model=self.load_cells_action_model),
+                SaveCellsAction(model=self.save_cells_action_model),
+            ]
+        )
+
--- regexport/main.py
+++ regexport/main.py
@@ -2,65 +2,15 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-from regexport.actions.download_biop_extensions import SaveBiopExtensionsAction, SaveBiopExtensionsActionModel
-from regexport.actions.load_cells import LoadCellsAction, LoadCellsActionModel
-from regexport.actions.load_atlas import LoadAtlasActionModel, LoadAtlasAction
-from regexport.actions.save_cells import SaveCellsAction, SaveCellsActionModel
-from regexport.actions.save_script import SaveGroovyScriptAction, SaveGroovyScriptActionModel
-from regexport.views.histogram import HistogramView, HistogramModel
-from regexport.views.main_window import MainWindow
-from regexport.model import AppState
-from regexport.views.plot_3d import PlotterModel, PlotterView
-from regexport.views.region_tree import BrainRegionTree, BrainRegionTreeViewModel
-from regexport.views.sidebar import Sidebar, SidebarModel
-from regexport.views.text_selector import TextSelectorModel, DropdownTextSelectorView
+from regexport.app import App
 
 
 def main():
-    global model
-    model = AppState()
-    app = QApplication(sys.argv)
-    plotter_model = PlotterModel()
-    plotter_model.register(model=model)
-    brain_region_tree_model = BrainRegionTreeViewModel()
-    brain_region_tree_model.register(model=model)
-    load_atlas_action_model = LoadAtlasActionModel()
-    load_atlas_action_model.register(model=model)
-    load_cells_action_model = LoadCellsActionModel()
-    load_cells_action_model.register(model=model)
-    save_cells_action_model = SaveCellsActionModel()
-    save_cells_action_model.register(model=model)
-    colormap_selector_model = TextSelectorModel()
-    colormap_selector_model.register(model=model, options_attr='colormap_options', selected_attr='selected_colormap')
-    colordata_selector_model = TextSelectorModel()
-    colordata_selector_model.register(model=model, options_attr='column_to_plot_options', selected_attr='column_to_plot')
-
-    sidebar_model = SidebarModel()
-    histogram_model = HistogramModel()
-    histogram_model.register(model=model)
-    win = MainWindow(
-        main_widgets=[
-            BrainRegionTree(model=brain_region_tree_model),
-            PlotterView(model=plotter_model),
-            Sidebar(
-                model=sidebar_model,
-                widgets=[
-                    DropdownTextSelectorView(model=colordata_selector_model),
-                    DropdownTextSelectorView(model=colormap_selector_model),
-                    HistogramView(model=histogram_model),
-                ]
-            ),
-        ],
-        menu_actions=[
-            SaveBiopExtensionsAction(model=SaveBiopExtensionsActionModel()),
-            SaveGroovyScriptAction(model=SaveGroovyScriptActionModel()),
-            LoadAtlasAction(model=load_atlas_action_model),
-            LoadCellsAction(model=load_cells_action_model),
-            SaveCellsAction(model=save_cells_action_model),
-        ]
-    )
+    qapp = QApplication(sys.argv)
+    app = App()
+    win = app.create_gui()
     win.show()
-    sys.exit(app.exec_())
+    sys.exit(qapp.exec_())
 
 
 if __name__ == '__main__':
