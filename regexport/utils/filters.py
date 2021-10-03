d4e2e859f578356049b43aa84211ff4f6a958322
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-10-03 15:55:20 +0200

    pulled Brainglobe out of filters.is_parent

--- regexport/utils/filters.py
+++ regexport/utils/filters.py
@@ -1,17 +1,16 @@
 from typing import Tuple
 
-from bg_atlasapi import BrainGlobeAtlas
 from pandas import Series
+from treelib import Tree
 
 
-def is_parent(id: Series, selected_ids: Tuple[int, ...], atlas: BrainGlobeAtlas) -> bool:
+def is_parent(id: Series, selected_ids: Tuple[int, ...], tree: Tree) -> bool:
     assert len(id.unique()) == 1
     id = int(id.values[0])
     if id != 0:
         if id in selected_ids:
             return True
         else:
-            is_ancestor = atlas.hierarchy.is_ancestor
-            return any(is_ancestor(selected_id, id) for selected_id in selected_ids)
+            return any(tree.is_ancestor(selected_id, id) for selected_id in selected_ids)
     else:
         return False
